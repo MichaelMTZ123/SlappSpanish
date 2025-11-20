b417941be2f21a17039b7cd2fce1c7f691900656
BD322 <michael.tz2008@gmail.com>
2025-11-20 18:59:31 +0200

    Add Android support and update gitignore

---
+++ android/app/src/main/java/com/slapp/spanish/MainActivity.java
@@ -0,0 +1,5 @@
+package com.slapp.spanish;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.slapp.spanish',
+  appName: 'SlappSpanish',
+  webDir: 'dist'
+};
+
+export default config;
